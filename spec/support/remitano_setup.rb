4422e8a882d98c81d21517ffed33e999419a1b40
Phuong Nguyen <phuongnd08@gmail.com>
2015-03-09 16:16:51 +0700

    Clean up setup_remitano

--- spec/support/remitano_setup.rb
+++ spec/support/remitano_setup.rb
@@ -4,14 +4,14 @@ RSpec.configure do |config|
     Remitano.setup do |config|
       config.key = nil
       config.secret = nil
-      config.client_id = nil
     end
   end
 end
 
 def setup_remitano
   Remitano.setup do |config|
-    raise "You must set environment variable REMITANO_KEY and REMITANO_SECRET with your username and password to run specs." if ENV['REMITANO_KEY'].nil? or ENV['REMITANO_SECRET'].nil?
+    raise "REMITANO_KEY is not set" unless ENV["REMITANO_KEY"].present?
+    raise "REMITANO_SECRET is not set" unless ENV["REMITANO_SECRET"].present?
     config.key = ENV['REMITANO_KEY']
     config.secret = ENV['REMITANO_SECRET']
   end
