88f2b87f821091949f75dd3182a032273b83b397
Phuong Nguyen <phuongnd08@gmail.com>
2015-04-13 10:20:59 +0700

    Trim more fat

--- lib/remitano.rb
+++ lib/remitano.rb
@@ -27,53 +27,6 @@ module Remitano
     @@orders ||= Remitano::Orders.new
   end
 
-  def self.user_transactions
-    self.sanity_check!
-
-    @@transactions ||= Remitano::UserTransactions.new
-  end
-
-  def self.transactions
-    return Remitano::Transactions.from_api
-  end
-
-  def self.balance
-    self.sanity_check!
-
-    JSON.parse Remitano::Net.post('/balance').to_str
-  end
-
-  def self.withdraw_bitcoins(options = {})
-    self.sanity_check!
-    if options[:amount].nil? || options[:address].nil?
-      raise MissingConfigExeception.new("Required parameters not supplied, :amount, :address")
-    end
-    response_body = Remitano::Net.post('/bitcoin_withdrawal',options).body_str
-    if response_body != 'true'
-      return JSON.parse response_body
-    else
-      return response_body
-    end
-  end
-  def self.bitcoin_deposit_address
-    # returns the deposit address
-    self.sanity_check!
-    return Remitano::Net.post('/bitcoin_deposit_address').body_str
-  end
-
-  def self.unconfirmed_user_deposits
-    self.sanity_check!
-    return JSON.parse Remitano::Net::post("/unconfirmed_btc").body_str
-  end
-
-  def self.ticker
-    return Remitano::Ticker.from_api
-  end
-
-  def self.order_book
-    return JSON.parse Remitano::Net.get('/order_book').to_str
-  end
-
   def self.configure
     yield self
   end
