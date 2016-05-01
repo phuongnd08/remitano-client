4260c04738e19235a8bb8a9cf2c87b381f7d46d6
Phuong Nguyen <phuongnd08@gmail.com>
2016-05-01 14:21:46 +0700

    Correct my_offers retrieval

--- lib/remitano/offers.rb
+++ lib/remitano/offers.rb
@@ -2,7 +2,7 @@ require_relative "collection"
 module Remitano
   class Offers < Remitano::Collection
     def my_offers(side)
-      Remitano::Net.get("/offers?offer_type=#{side}").execute.offers
+      Remitano::Net.get("/offers/my_offers?offer_type=#{side}").execute.offers
     end
 
     def update(offer_id, params)
