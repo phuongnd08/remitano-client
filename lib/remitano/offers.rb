88d4eaa6e00900a3d53a15facdc70327070e8a39
Phuong Nguyen <phuongnd08@gmail.com>
2016-05-01 14:47:50 +0700

    Add enable/disable offer

--- lib/remitano/offers.rb
+++ lib/remitano/offers.rb
@@ -8,5 +8,13 @@ module Remitano
     def update(offer_id, params)
       Remitano::Net.patch("/offers/#{offer_id}", params).execute
     end
+
+    def enable(offer_id)
+      Remitano::Net.patch("/offers/#{offer_id}/enable").execute
+    end
+
+    def disable(offer_id)
+      Remitano::Net.patch("/offers/#{offer_id}/disable").execute
+    end
   end
 end
