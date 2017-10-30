bd806679e3c57d8b98caefd7e397b0727754f2f3
Phuong Nguyen <phuongnd08@gmail.com>
2017-10-30 14:33:22 +0700

    Add coin_currency to offer update

--- lib/remitano/offers.rb
+++ lib/remitano/offers.rb
@@ -6,16 +6,16 @@ module Remitano
     end
 
     def update(offer_id, params)
-      response = Remitano::Net.patch("/offers/#{offer_id}", params).execute
+      response = Remitano::Net.patch("/offers/#{offer_id}?coin_currency=#{coin}", params).execute
       Remitano.action_confirmations.confirm_if_neccessary!(response)
     end
 
     def enable(offer_id)
-      Remitano::Net.patch("/offers/#{offer_id}/enable").execute
+      Remitano::Net.patch("/offers/#{offer_id}/enable?coin_currency=#{coin}").execute
     end
 
     def disable(offer_id)
-      Remitano::Net.patch("/offers/#{offer_id}/disable").execute
+      Remitano::Net.patch("/offers/#{offer_id}/disable?coin_currency=#{coin}").execute
     end
   end
 end
