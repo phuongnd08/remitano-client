2d77ced92ea5bdb2a430cc55ceb17625aefb7f0b
Phuong Nguyen <phuongnd08@gmail.com>
2017-10-30 10:20:11 +0700

    Correct offers spec

--- spec/offers_spec.rb
+++ spec/offers_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Remitano::Offers do
   describe :my_offers, vcr: {cassette_name: 'remitano/my_offers/sell'} do
-    subject { Remitano.offers.my_offers("sell") }
+    subject { Remitano.offers.btc.my_offers("sell") }
     its(:length) { should == 1 }
   end
 
   describe :update, vcr: {cassette_name: 'remitano/offers/update'} do
     it "change the offer" do
-      Remitano.offers.update(2, price: 21800)
-      offer = Remitano.offers.my_offers("sell").first
+      Remitano.offers.btc.update(2, price: 21800)
+      offer = Remitano.offers.btc.my_offers("sell").first
       offer[:price].should == 21800
     end
   end
